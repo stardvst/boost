8c8596d73a74445160da5c2f088939e1387e62e7
Lilia <lilia.manukyan@hotmail.com>
2018-07-10 23:16:07 +0400

    make_shared

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-#include <boost/shared_ptr.hpp>
-#include <Windows.h>
+#include <typeinfo>
+#include <boost/make_shared.hpp>
 
 int main()
 {
-	boost::shared_ptr<int> p1{ new int {1} };
-	std::cout << *p1 << '\n';
+	auto p1 = boost::make_shared<int>(1);
+	std::cout << typeid(p1).name() << '\n';
 
-	boost::shared_ptr<int> p2{ p1 };
-	std::cout << *p2 << '\n';
-
-	p1.reset(new int{ 2 });
-	std::cout << *p1.get() << '\n';
-
-	p1.reset();
-
-	std::cout << std::boolalpha << static_cast<bool>(p2) << '\n';
-
-	// deleter
-	boost::shared_ptr<void> handle{ OpenProcess(PROCESS_SET_INFORMATION, FALSE, GetCurrentProcessId()), CloseHandle() };
+	auto p2 = boost::make_shared<int[]>(10);
+	std::cout << typeid(p2).name() << '\n';
 	
 	std::cin.get();
 	return 0;

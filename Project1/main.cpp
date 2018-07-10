17e5f61dc076e668ba5f05e56b4b5d13bfe8446d
Lilia <lilia.manukyan@hotmail.com>
2018-07-10 22:04:06 +0400

    scoped ptr : copy error

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <boost/scoped_ptr.hpp>
 
+// const & is ok
+void f(boost::scoped_ptr<int> ptr)
+{
+	std::cout << *ptr << '\n';
+}
+
 int main()
 {
 	boost::scoped_ptr<int> p{ new int{ 1 } };
-	std::cout << *p << '\n';
 
-	p.reset(new int{ 2 });
-	std::cout << *p.get() << '\n';
-
-	p.reset();
-	std::cout << std::boolalpha << static_cast<bool>(p) << '\n';
+	// error
+	f(p);
 
 	std::cin.get();
 	return 0;

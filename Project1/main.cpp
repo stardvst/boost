e3769af2fcca7016c17a5971db17926666052882
Lilia <lilia.manukyan@hotmail.com>
2018-07-10 22:31:00 +0400

    scoped array

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <boost/scoped_ptr.hpp>
-
-// const & is ok
-void f(boost::scoped_ptr<int> ptr)
-{
-	std::cout << *ptr << '\n';
-}
+#include <boost/scoped_array.hpp>
+#include <boost/range.hpp>
 
 int main()
 {
-	boost::scoped_ptr<int> p{ new int{ 1 } };
+	boost::scoped_array<int> p{ new int[2] };
+	*p.get() = 1;
+	p[1] = 2;
 
-	// error
-	f(p);
+	auto ptr = p.get();
+	for (auto it : boost::make_iterator_range(ptr, ptr + 2))
+		std::cout << it << ' ';
+
+	p.reset(new int[3]);
+
+	// can't get array's size, oops
 
 	std::cin.get();
 	return 0;

b0049fdc1e87084967b371e231e52a008cf96cc5
Lilia <lilia.manukyan@hotmail.com>
2018-07-10 21:59:56 +0400

    scoped ptr

---
+++ Project1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <boost/scoped_ptr.hpp>
+
+int main()
+{
+	boost::scoped_ptr<int> p{ new int{ 1 } };
+	std::cout << *p << '\n';
+
+	p.reset(new int{ 2 });
+	std::cout << *p.get() << '\n';
+
+	p.reset();
+	std::cout << std::boolalpha << static_cast<bool>(p) << '\n';
+
+	std::cin.get();
+	return 0;
+}

813702712f11aad804e8549d8b5724df73151c61
Lilia <lilia.manukyan@hotmail.com>
2018-07-10 23:00:42 +0400

    shared ptr

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <boost/scoped_array.hpp>
-#include <boost/range.hpp>
+#include <boost/shared_ptr.hpp>
+#include <Windows.h>
 
 int main()
 {
-	boost::scoped_array<int> p{ new int[2] };
-	*p.get() = 1;
-	p[1] = 2;
+	boost::shared_ptr<int> p1{ new int {1} };
+	std::cout << *p1 << '\n';
 
-	auto ptr = p.get();
-	for (auto it : boost::make_iterator_range(ptr, ptr + 2))
-		std::cout << it << ' ';
+	boost::shared_ptr<int> p2{ p1 };
+	std::cout << *p2 << '\n';
 
-	p.reset(new int[3]);
+	p1.reset(new int{ 2 });
+	std::cout << *p1.get() << '\n';
 
-	// can't get array's size, oops
+	p1.reset();
 
+	std::cout << std::boolalpha << static_cast<bool>(p2) << '\n';
+
+	// deleter
+	boost::shared_ptr<void> handle{ OpenProcess(PROCESS_SET_INFORMATION, FALSE, GetCurrentProcessId()), CloseHandle() };
+	
 	std::cin.get();
 	return 0;
 }

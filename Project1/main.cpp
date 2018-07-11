84e04451579f8a9332c1872727729f1ba43822de
Lilia <lilia.manukyan@hotmail.com>
2018-07-11 22:55:24 +0400

    ptr container (vector)

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,32 +1,12 @@
 #include <iostream>
-#include <boost/intrusive_ptr.hpp>
-#include <atlbase.h>
-
-void intrusive_ptr_add_ref(IDispatch *pDispatch) { pDispatch->AddRef(); }
-void intrusive_ptr_release(IDispatch *pDispatch) { pDispatch->Release(); }
-
-void check_windows_folder()
-{
-	CLSID clsid;
-	CLSIDFromProgID(CComBSTR{ "Scripting.FileSystemObject" }, &clsid);
-
-	void *ptr;
-	CoCreateInstance(clsid, 0, CLSCTX_INPROC_SERVER, __uuidof(IDispatch), &ptr);
-	boost::intrusive_ptr<IDispatch> disp{ static_cast<IDispatch *>(ptr), false };
-
-	CComDispatchDriver dd{ disp.get() };
-	CComVariant arg{ "C:\\Windows" };
-	CComVariant ret{ false };
-
-	dd.Invoke1(CComBSTR{ "FolderExists" }, &arg, &ret);
-	std::cout << std::boolalpha << (ret.boolVal != 0) << '\n';
-}
+#include <boost/ptr_container/ptr_vector.hpp>
 
 int main()
 {
-	CoInitialize(nullptr);
-	check_windows_folder();
-	CoUninitialize();
+	boost::ptr_vector<int> v;
+	v.push_back(new int{ 1 });
+	v.push_back(new int{ 2 });
+	std::cout << v.back() << '\n';
 
 	std::cin.get();
 	return 0;

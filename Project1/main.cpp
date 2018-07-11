8dd77ea4b57d97da45aef93e846cbab5182a3b8a
Lilia <lilia.manukyan@hotmail.com>
2018-07-11 23:00:47 +0400

    ptr_set vs std::set

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/ptr_container/ptr_set.hpp>
+#include <boost/ptr_container/indirect_fun.hpp>
+#include <set>
+#include <memory>
+#include <functional>
 
 int main()
 {
-	boost::ptr_vector<int> v;
-	v.push_back(new int{ 1 });
-	v.push_back(new int{ 2 });
-	std::cout << v.back() << '\n';
+	boost::ptr_set<int> s;
+	s.insert(new int{ 2 });
+	s.insert(new int{ 1 });
+	std::cout << *s.begin() << '\n';
+
+	std::set<std::unique_ptr<int>, boost::indirect_fun<std::less<>>> v;
+	v.insert(std::make_unique<int>(int{ 2 }));
+	v.insert(std::make_unique<int>(int{ 1 }));
+	std::cout << **v.begin() << '\n';
 
 	std::cin.get();
 	return 0;

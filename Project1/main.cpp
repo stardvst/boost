c15ae42fbf9ff6f6ab282235c56105b5c5fd648f
Lilia <lilia.manukyan@hotmail.com>
2019-01-30 13:36:23 +0400

    fibonacci (binet's formula)

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,32 +1,41 @@
+#include <cassert>
+#include <cstdlib>
+#include <cstdint>
 #include <iostream>
-#include <boost/scope_exit.hpp>
+#include <limits>
+#include <string>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
-struct x
+using real_t = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<10000>>;
+
+real_t operator"" _R(long double d)
 {
-	int i;
+	return real_t{ d };
+}
 
-	void foo()
-	{
-		i = 10;
-		BOOST_SCOPE_EXIT(void)
-		{
-			std::cout << "last\n";
-		} BOOST_SCOPE_EXIT_END
-		
-		BOOST_SCOPE_EXIT(this_)
-		{
-			this_->i = 20;
-			std::cout << "first\n";
-		} BOOST_SCOPE_EXIT_END
-	}
-};
-
-int main()
+real_t get_fibonacci(uintmax_t const n)
 {
-	x obj;
-	obj.foo();
-	std::cout << obj.i << '\n';
+	// Use Binet's formula, limited n to decimal for perf increase
+	if (n < 1)
+		return 0.0_R;
+
+	static real_t const sqrt_five = sqrt(5.0_R);
+	static real_t const a_part = (1.0_R + sqrt_five) / 2.0_R;
+	static real_t const b_part = (1.0_R - sqrt_five) / 2.0_R;
+
+	real_t const a = pow(a_part, n);
+	real_t const b = pow(b_part, n);
+	return round((a - b) / sqrt_five);
+}
+
+int main(int argc, char **argv)
+{
+	//	assert( argc == 2 );
+	//
+	//	auto n = strtoull( argv[1], 0, 10 );
+	uint64_t n = 15;
+	std::cout << n << ": " << std::setprecision(std::numeric_limits<real_t>::max_digits10) << get_fibonacci(n) << '\n';
 
 	std::cin.get();
-	return 0;
+	return EXIT_SUCCESS;
 }
